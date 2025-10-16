f80514b2f5a767768129e280f0437f635fc76b7a
Ekta <ektasinha2535@gmail.com>
2025-10-16 16:20:16 +0530

    assignment 2.3

---
+++ assignment2.3/src/com/sunbeam/DateTest.java
@@ -0,0 +1,13 @@
+package com.sunbeam;
+
+public class DateTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date d1 = new Date(25,06,2002);
+		Date d2 = new Date(19,05,2001);
+		d1.displayDate();
+		d2.displayDate();
+	}
+
+}
